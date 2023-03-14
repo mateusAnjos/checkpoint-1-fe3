e7deeee02e099b7f63b90fc9322ed778ba90d48f
mateusAnjos <mateus.anjosdefaria@gmail.com>
2023-03-14 01:15:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React To-do List 
+## Checkpoint #1 Front End III
+
+### Integrantes
+    [Mateus Anjos](https://github.com/mateusAnjos/)
+    [Bruno Zeulli](https://github.com/bczeulli/)
+    [Osmar Franco](https://github.com/osmarfranco/)
+    [Renato Lino](https://github.com/renatoljohn/)
+    [Joao Fauth](https://github.com/joaofauth/)
+
+## Objetivo
+    Projeto criado para primeira avaliação da matéria Front End 3.
+    Design usitlizado: [Figma](https://www.figma.com/file/R7avncoTK3oAiAdGpepXBE/Untitled?node-id=0%3A1&t=jkGmYxeTVLnX4cUh-0)
---
+++ src/App.jsx
@@ -0,0 +1,184 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+ const [id, setId] = useState("");
+ const [title, setTitle] = useState("");
+ const [category, setCategory] = useState("");
+ const [date, setDate] = useState("");
+ const [description, setDescription] = useState("");
+
+ const [validateTitle, setValidateTitle] = useState("");
+ const [validateCategory, setValidateCategory] = useState("");
+ const [validateDate, setValidateDate] = useState("");
+ const [validateDescription, setValidateDescription] =useState("");
+ const [taskList, setTaskList] = useState([]); 
+
+ 
+
+
+function addItem(event){
+  event.preventDefault();
+  
+  if(validate()){
+    return
+  }
+
+  function validate(){
+    let validado = false;
+    if(title === ""){
+      setValidateTitle("Campo Obrigatório!")
+      validado = true;
+    }else {
+      setValidateTitle("");
+    }
+
+    if(category === "" ){
+      setValidateCategory("Campo Obrigatório!")
+      validado = true
+    }else { setValidateCategory('');
+    }
+
+    if(date === "" ){
+      setValidateDate("Campo Obrigatório!")
+      validado = true
+    }else {
+      setValidateDate("");
+    }
+    if(description === "" ){
+      setValidateDescription("Campo Obrigatório!")
+      validado = true
+    }else {
+      setValidateDescription("");
+    }
+    return validado;
+  }
+  
+  if(id){
+  const copyTaskList = [...taskList];
+
+  const index = copyTaskList.findIndex((task)=> task.id === id);
+
+  copyTaskList[index].title = title;
+  copyTaskList[index].category = category;
+  copyTaskList[index].date = date;
+  copyTaskList[index].description = description;
+
+  setTaskList(copyTaskList);
+
+  } else{
+    setTaskList([...taskList, 
+      {
+        id: Date.now(),
+        title: title,
+        category: category,
+        date: date,
+        description: description,    
+      },
+    ]);
+  }
+
+  
+
+setId("");
+setTitle("");
+setCategory("");
+setDate("");
+setDescription("");
+
+setValidateCategory('');
+setValidateDate('');
+setValidateDescription('');
+
+}
+
+
+function deleteItem(id){
+  if(confirm("Deseja realmente apagar esse card?")) {
+    const result = taskList.filter((item)=>item.id !== id);
+    setTaskList(result);
+  }
+}
+
+function stateFiller(item){
+  setId(item.id);
+  setTitle(item.title);
+  setCategory(item.category);
+  setDate(item.date);
+  setDescription(item.description);
+}
+
+
+
+  
+  return (
+    
+    <div className="container">
+      <div className="boxCadastro">
+        <form onSubmit={addItem}>
+         <h2 id="formTitle">{id ? "Editar" : "Cadastrar"} Tarefa</h2>
+
+         <input type="text" value={title} placeholder='Título' maxLength={30}  onChange={(event) => setTitle(event.target.value)}/>
+         <small id='validateTitle'>{validateTitle}</small>
+
+        <select name="categorias"   value={category}  id="cat" onChange={(event)=>setCategory(event.target.value)}>
+          <option value="" disabled selected>Selecione a Categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Prioridade">Prioridade</option>
+          <option value="Outros">Outros</option>
+        </select>
+        <small>{validateCategory}</small>
+        
+        <input type="date" value={date} placeholder="Data" onChange={(event) => setDate(event.target.value)} />
+        <small>{validateDate}</small>
+        
+        <input placeholder="Descrição" value={description} type="text" maxLength={200} onChange={(event) => setDescription(event.target.value)}/>
+        <small>{validateDescription}</small>
+        
+
+        <input type="submit" value={id ? "Salvar" : "Cadastrar"}  />
+        </form>
+      </div>
+      
+      <div className='boxTarefas'>
+        <header>
+          <h1>Minhas Tarefas</h1>
+          <p>total: {taskList.length} tarefas</p>
+        </header>
+        
+        {taskList.length > 0 ?(
+          <ul>
+          {taskList.map((item, index) => (
+            <li key={index}>
+              <div className='card'>
+                <div className='cardLeft'>
+                  <h2 className='cardTitle'>{item.title}</h2>
+                  <h3 className='cardCategory'>{item.category}</h3>
+                  <p className='cardDescription'>{item.description}</p>
+                </div>
+             
+            
+              <div className='cardRight'>
+                <p className='cardDate'> {item.date.split('-').reverse().join('/')} </p>
+                <div className="btns">
+                  <button className='editBtn'onClick={() => stateFiller(item)}></button>
+                  <button className='deleteBtn' onClick={() => deleteItem(item.id)}></button>
+                </div>
+              </div>
+              </div>
+            </li>
+          ))}
+          </ul>
+        ) : (
+          <p>Nenhum card cadastrado</p>
+        )}
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
